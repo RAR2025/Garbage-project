544c7c76e28f94aac421982283957dc6ee1684c4
Ruturaj Amit Rajwade <ruturaj.rajwade@gmail.com>
2026-05-30 14:47:08 +0530

    chore: admin panel configuration

--- route_manager/admin.py
+++ route_manager/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 from .models import Shop, Edge
 
-# Register your models here.
+class EdgeInline(admin.TabularInline):
+    model = Edge
+    fk_name = "from_shop"
+    extra = 0
+    fields = ("to_shop", "distance_km")
+
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
     list_display = ("name", "garbage_weight", "is_active", "lat", "lng")
     list_filter = ("is_active",)
-    search_fields =("name",)
-    ordering =("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+    inlines = (EdgeInline,)
 
 @admin.register(Edge)
 class EdgeAdmin(admin.ModelAdmin):
-    list_display=("from_shop", "to_shop", "distance_km")
-    list_filter=("distance_km",)
+    list_display = ("from_shop", "to_shop", "distance_km")
+    list_filter = ("from_shop", "to_shop")
+    search_fields = ("from_shop__name", "to_shop__name")
