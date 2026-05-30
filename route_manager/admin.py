1aa239795df0fddfdf25da9d989db9f4ad18e751
Ruturaj Amit Rajwade <ruturaj.rajwade@gmail.com>
2026-05-30 14:43:36 +0530

    chore: database and model setup

--- route_manager/admin.py
+++ route_manager/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Shop, Edge
 
 # Register your models here.
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ("name", "garbage_weight", "is_active", "lat", "lng")
+    list_filter = ("is_active",)
+    search_fields =("name",)
+    ordering =("name",)
+
+@admin.register(Edge)
+class EdgeAdmin(admin.ModelAdmin):
+    list_display=("from_shop", "to_shop", "distance_km")
+    list_filter=("distance_km",)
--- route_manager/models.py
+++ route_manager/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=255)
+    lat = models.FloatField()
+    lng = models.FloatField()
+    garbage_weight = models.FloatField(default= 0.0)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Edge(models.Model):
+    from_shop = models.ForeignKey(
+        Shop,
+        on_delete=models.CASCADE,
+        related_name="outgoing_edge",
+    )
+    to_shop = models.ForeignKey(
+        Shop,
+        on_delete=models.CASCADE,
+        related_name="incomming_edge",
+    )
+    distance_km = models.FloatField()
+
+    def __str__(self) :
+        return f"{self.from_shop} -> {self.to_shop} ({self.distance_km} km)"
+
+    def clean(self):
+        if self.to_shop == self.from_shop:
+            raise ValidationError(
+                "source and Destination shops cannot be same"
+            )
