5225891c5a6167caad9d5ece391cb6a2322ef777
Ruturaj Amit Rajwade <ruturaj.rajwade@gmail.com>
2026-06-01 19:28:13 +0530

    feat: shop list and detail views

--- route_manager/urls.py
+++ route_manager/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+
+
 urlpatterns = [
     path('', views.index),
+    path("shops/", views.ShopListJsonView.as_view(), name="shop-list")
 ]
--- route_manager/views.py
+++ route_manager/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
+from .models import Shop
 # Create your views here.
 def index(request):
     # templates/index.html exists at project root templates/, so render that
     return render(request, 'index.html')
+
+class ShopListJsonView(View):
+    def get(self, request, *args, **kwargs):
+        shops = list(Shop.objects.values(
+            "id",
+            "name",
+            "lat",
+            "lng",
+            "garbage_weight",
+            "is_active",
+        ))
+        return JsonResponse(shops, safe=False) 
